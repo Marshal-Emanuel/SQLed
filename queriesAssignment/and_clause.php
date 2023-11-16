8a05cf4bc8ca637f5c308a207cea36adc8c78ef8
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-11-16 23:33:39 +0300

    Update and_clause.php

--- queriesAssignment/and_clause.php
+++ queriesAssignment/and_clause.php
@@ -17,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Form data
+// Form data recieved
 $tableName = $_POST['txtTableNameAnd'];
 $condition1 = $_POST['txtCondition1And'];
 $condition2 = $_POST['txtCondition2And'];
