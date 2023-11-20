741bab4680c9496e2200d788ac9f0d678c385342
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-11-21 02:03:39 +0300

    Update create_table.php

--- queriesAssignment/create_table.php
+++ queriesAssignment/create_table.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
 $tableName = $_POST['txtTableNameCreate'];
 $column1Name = $_POST['txtColumn1Name'];
 $column1Type = $_POST['ddlColumn1Type'];
-$column2Name = $_POST['txtColumn2Name'];
+$column2Name = $_POST['txtColumn2Name']; 
 $column2Type = $_POST['ddlColumn2Type'];
 
 // Construct the CREATE TABLE query 
