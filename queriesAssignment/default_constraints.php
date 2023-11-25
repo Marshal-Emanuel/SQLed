bf071c555034983a627d4785d939dde1dfe55fc0
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-11-26 02:48:41 +0300

    Update default_constraints.php

--- queriesAssignment/default_constraints.php
+++ queriesAssignment/default_constraints.php
@@ -6,7 +6,7 @@ $dbname = "supermarket_management";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+ 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
